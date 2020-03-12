e905bec40b6e785b5dfe4b3b923db62e0a8213d8
xmagnetisx <muhammad.trad@yahoo.com>
2020-03-11 19:30:23 -0700

    added javascript

---
+++ app.js
@@ -0,0 +1,41 @@
+const todoInputEl = document.querySelector(".todo__input");
+const todoListEl = document.querySelector(".todo__list");
+const todoItemEls = document.querySelectorAll(".todo__item");
+
+function addListItem(){
+    todoInputEl.addEventListener("keypress", function(event){
+        if (event.keyCode === 13){
+            let newListItem = createListItem(todoInputEl.value);
+            todoListEl.insertBefore(newListItem, todoListEl.childNodes[0]);
+            //todoListEl.appendChild(newListItem);
+            todoInputEl.value = '';
+        }
+    })
+}
+//does not work. Due to bubbling and propagation
+// function toggleDone(){
+//     for (let elem of todoItemEls){
+//         elem.addEventListener("click", function(){
+//             elem.classList.toggle("done");
+//         })
+//     }
+// }
+
+function toggleDone(){
+    todoListEl.addEventListener("click", function(event){
+        if (event.target.classList.contains("todo__item")){
+        event.target.classList.toggle("done");
+        }
+    })
+}
+
+
+function createListItem(text){
+    const newListElement = document.createElement("li");
+    newListElement.textContent = text;
+    newListElement.setAttribute("class", "todo__item")
+    return newListElement;
+}
+
+toggleDone();
+addListItem();
